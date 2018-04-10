f72fc3a14523773ba8e4750b6ffa129b86ef8ad4
JamesMcAvoy <mathieu.brisset@laposte.net>
2018-04-10 17:06:27 +0200

    logs
    Affiche les logs lors d'une requête / erreurs si 500

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Site web développé à partir de la bibliothèque ReactPHP. Serveur PHP asynchr
 1. Installer les dépendances : ```composer install```
 2. Exécuter ```schema.sql``` dans la base de données
 3. Éditer config.json commu voulu
-4. Lancer le serveur : ```./start``` (l'arrêter : ```./stop```, sous Windows directement : ```php src/phpttpd```)
+4. Lancer le serveur : ```./start.sh``` (l'arrêter : ```./stop.sh```, sous Windows directement : ```php src/phpttpd```)
--- src/controllers/Middleware/HttpLogsMiddleware.php
+++ src/controllers/Middleware/HttpLogsMiddleware.php
@@ -2,13 +2,26 @@
 
 namespace Controllers\Middleware;
 
+use React\Http\Io\ServerRequest as Request,
+    React\Http\Response;
+
 final class HttpLogsMiddleware {
 
-    public function __construct() {
+    /**
+     * Display a log from a request to the server
+     * @param React\Http\Io\ServerRequest   $request
+     * @param React\Http\Response           $response
+     * @param String                        $msg
+     * @return Response
+     */
+    public static function log(Request $request, Response $response, String $msg = '') : Response {
 
-    }
-
-    public function __invoke() {
+        $date = date('d/m H:i:s');
+        $ip = $request->getServerParams()['REMOTE_ADDR'];
+        $path = $request->getUri()->getPath();
+        $status = $response->getStatusCode();
+        echo "$date : $ip $path $status $msg\n";
+        return $response;
 
     }
 
