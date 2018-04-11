14e38c97cc9be2b127d99d72a5ababacb903aba8
Blackfish <mathieu.brisset@laposte.net>
2018-04-11 22:39:50 +0200

    register

--- src/controllers/Controller/IndexController.php
+++ src/controllers/Controller/IndexController.php
@@ -8,7 +8,7 @@ final class IndexController extends Controller {
 
     public static function index($req, $res) {
 
-        return self::render($res, 'index', ['hello' => 'ÉcOlE d\'ingénieurs']);
+        return self::render($res, 'inscription');
 
     }
     
