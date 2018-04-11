38b24fa11f22cee172e5165d691bb59c9c5152a6
BuetThomas <thomas.buet@viacesi.fr>
2018-04-11 11:14:55 +0200

    index.blade.php

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,15 +1,67 @@
 <!doctype html>
 <html>
     <head>
+
         <meta charset="utf-8" />
         <meta name="description" content="bde" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>BDE</title>
         <link rel="stylesheet" href="css/style.css" />
         <link rel="icon" href="favicon.ico" />
+
     </head>
+    
+
+
     <body>
-        <p>{{ $hello }} eXiA cEsI</p>
-        <script src="js/main.js"></script>
+
+
+
+    <header>
+
+        <img src="/img/exialogo.png"/>
+        <nav>
+            <ul>
+                        <li><a href="">Accueil</a></li>
+                        <li><a href="">Evenements</a></li>
+                        <li><a href="">Boutique</a></li>
+                        <li><a href="">Connexion</a></li>
+                        <li><a href="">Inscription</a></li>
+                    </ul>
+        </nav>
+    </header>
+
+        <div class="baniere">
+            <h1>BDE EXIA STRASBOURG</h1>
+        </div>
+
+
+        <div>
+            <p>{{ $hello }} eXiA cEsI</p>
+            <script src="js/main.js"></script>
+            <p>
+                bonjour
+            </p>
+        </div>
+
+
+    <section>
+
+        <article>
+            <p>evenement du mois</p>
+        </article>
+
+        <aside>
+            <p>items les plus acheter</p>
+        </aside>
+
+    </section>
+
+    <footer>
+        <p>copyright <br>
+
+    MathouDreamTeam</p>
+    </footer>
+
     </body>
 </html>
