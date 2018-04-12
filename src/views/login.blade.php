cfd1133bf489317a1e8e80761d5fe0e5985dd594
JamesMcAvoy <mathieu.brisset@laposte.net>
2018-04-12 16:12:12 +0200

    front end js

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -7,15 +7,15 @@
 	@section('main_content')	
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
-		<form action="/register" method="post">
+		<form action="/login" method="post">
 		  <div class="form-group row">
 			<div class="col-sm-2">
 			</div>
 			<label for="EmailConnexion" class="col-sm-2 col-form-label">Email</label>
 			<div class="col-sm-6">
-			  <input type="email" class="form-control is-valid" id="EmailConnexion" placeholder="Email" name="courriel">
-			  <div class="valid-feedback">
-				Cette addresse e-mail existe
+			  <input type="email" class="form-control" id="EmailConnexion" placeholder="Email" name="courriel">
+			  <div class="invalid-feedback">
+				
 			  </div>
 			</div>
 		  </div>
@@ -24,9 +24,9 @@
 			</div>
 			<label for="MotDePasseConnexion" class="col-sm-2 col-form-label">Mot de passe</label>
 			<div class="col-sm-6">
-			  <input type="password" class="form-control is-invalid" id="MotDePasseConnexion" placeholder="Mot de passe" name="mdp">
+			  <input type="password" class="form-control" id="MotDePasseConnexion" placeholder="Mot de passe" name="mdp">
 				<div class="invalid-feedback">
-					Mauvais mot de passe
+					
 				</div>
 			</div>
 		  </div>
@@ -34,7 +34,7 @@
 			<div class="col-sm-5">
 			</div>
 			<div class="col-sm-4">
-			  <button type="submit" class="btn btn-outline-dark">Connexion</button>
+			  <button type="submit" class="btn btn-outline-dark" id="SubmitConnexion">Connexion</button>
 			</div>
 		  </div>
 		</form>
@@ -42,7 +42,7 @@
 				<div class="col-sm-2">
 				</div>
 				<div class="col-sm-7">
-				<a href="/inscription">Pas encore inscrit ? Clique-ici pour t'inscrire !</a>
+				<a href="/register">Pas encore inscrit ? Clique-ici pour t'inscrire !</a>
 				</div>
 			</div>
 		</div>
--- src/views/template.blade.php
+++ src/views/template.blade.php
@@ -1,4 +1,3 @@
-	
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,10 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>@yield('title')</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
 </head>
 
 <body>	
@@ -34,7 +30,7 @@
 					<a class="nav-link" href="/boutique">Boutique</a>
 				  </li>
 				</ul>
-					<a id="btn_connexion" href="/connexion" class="btn btn-outline-light btn-lg " role="button" aria-pressed="true">Connexion</a>
+					<a id="btn_connexion" href="/login" class="btn btn-outline-light btn-lg " role="button" aria-pressed="true">Connexion</a>
 			  </div>
 			</nav>	
 		</header>
@@ -48,5 +44,9 @@
 		
 		</footer>
 	</div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<script src="/js/main.js"></script>
 </body>
 </html>
