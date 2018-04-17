00674d7c062c5fd616c50eab54821a4b2cfa005e
Loulouv <louis.vernet@viacesi.fr>
2018-04-17 10:26:59 +0200

    Migration des Controllers

--- src/controllers/Controller/EventController.php
+++ src/controllers/Controller/EventController.php
@@ -52,6 +52,88 @@ final class EventController extends Controller {
         return self::render($res,'events',$event);
     }
 
+
+    /**
+    * Creation
+    ********/   
+    public static function eventCreate($req, $res){
+
+        $session = parent::getSession($req)->getContents();
+        $params = $req->getQueryParams();
+
+        $post = $req->getParsedBody();       
+        
+        $date_time = htmlentities($post['date'])." ".htmlentities($post['hour']);
+
+        $event = Model\Event::create([
+            'event_title'       => htmlentities($post['event_title']),
+            'event'             => htmlentities($post['event']),
+            'event_price'       => htmlentities($post['event_price']),
+            'event_picture'     => htmlentities($post['event_picture']),
+            'start_date'        => $date_time,
+            'time'              => htmlentities($post['time']),
+            'time_between_each' => htmlentities($post['time_between_each']),
+            'event_number'      => htmlentities($post['event_number']),
+            'event_state'       => htmlentities($post['event_state'])
+        ]);
+       
+        return $res->withStatus(302)->withHeader('Location', '/CreateEvent');
+    }
+
+
+    /**
+    * Registration
+    ********/
+    public static function eventRegistration($req, $res){
+
+        $sessionUser = self::getSessionUser($req);
+        $params = $req->getQueryParams();
+
+        $event = Model\Event::where('event_id', '=', $params['event_id'])->first(); 
+        $user = $sessionUser['id'];
+
+        if( self::sessionUserActive($req) &&
+            empty(Model\Registered::where('event_id', '=', $event->event_id)) &&
+            empty(Model\Registered::where('user_id', '=', $user))
+            ) {
+                $registered = new Model\Registered;
+                $registered->event_id=$event->event_id;
+                $registered->user_id=$user;
+                $registered->save();
+        }
+
+        return $res->withStatus(302)->withHeader('Location', '/events');
+    }
+
+    /**
+    * Manage
+    ********/
+    /* faudrait auto compléter le forms avec les infos de la BDD sur l'évenement
+    $post = $req->getParsedBody(); 
+    $event = Model\Event::where('item_id', '=', $post['item_id'])->get()->first();
+    return self::render($res,'ManageEvent',$event);*/
+
+    public static function eventManage($req, $res){
+
+        $post = $req->getParsedBody();       
+        
+        $date_time = htmlentities($post['date'])." ".htmlentities($post['hour']);    
+
+        $user = Model\Event::update([
+            'event_title'       => htmlentities($post['event_title']),
+            'event'             => htmlentities($post['event']),
+            'event_price'       => htmlentities($post['event_price']),
+            'event_picture'     => htmlentities($post['event_picture']),
+            'start_date'        => $date_time,
+            'time'              => htmlentities($post['time']),
+            'time_between_each' => htmlentities($post['time_between_each']),
+            'event_number'      => htmlentities($post['event_number']),
+            'event_state'       => htmlentities($post['event_state'])
+        ])->first();
+
+        return $res->withStatus(302)->withHeader('Location', '/evenements');
+    }  
+
 /*
 évenements du moi visible pour tous
 visiteurs : Evenements visible pour tout visiteur
--- src/controllers/Controller/IdeaController.php
+++ src/controllers/Controller/IdeaController.php
@@ -71,4 +71,28 @@ final class IdeaController extends Controller {
 
     }
 
+    /**
+     * Vote
+     */
+    public static function ideaVote($req, $res){
+
+        $sessionUser = self::getSessionUser($req);
+        $params = $req->getQueryParams();
+
+        $idea = Model\Idea::where('idea_id', '=', $params['idea_id'])->first(); 
+        $user = $sessionUser['id'];
+
+        if( self::sessionUserActive($req) &&
+            empty(Model\Voted::where('idea_id', '=', $idea->idea_id)) &&
+            empty(Model\Voted::where('user_id', '=', $user))
+            ) {
+                $Voted = new Model\Registered;
+                $Voted->idea_id=$idea->idea_id;
+                $Voted->user_id=$user;
+                $Voted->save();
+        }
+        // la vue ideas n'existe pas encore
+        return $res->withStatus(302)->withHeader('Location', '/ideas');
+    }
+
 }
