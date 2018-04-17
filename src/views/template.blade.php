039218e4fa27f20f0ab65620da0205fbd35965e1
Pansera <vincent.pansera@viacesi.fr>
2018-04-17 11:32:47 +0200

    Ajout fonction like boite à idée

--- src/views/template.blade.php
+++ src/views/template.blade.php
@@ -33,9 +33,9 @@
   					</a>
   					<div class="collapse" id="collapseExample">
   						<div class="row card-body">
-   						 <a class="col-md-4 nav-link bouton_event" href="/events#events">Événements</a>
-   						 <a class="col-md-4 nav-link bouton_event" href="/events#ideas">Boite à idées</a>
-   						 <a class="col-md-4 nav-link bouton_event" href="/events#form">Proposer vos idées</a>
+   						 <a id="bouton_events" class="col-md-4 nav-link" href="/events#events">Événements</a>
+   						 <a id="bouton_ideas" class="col-md-4 nav-link" href="/events#ideas">Boite à idées</a>
+   						 <a id="bouton_form" class="col-md-4 nav-link" href="/events#form">Proposer vos idées</a>
  						 </div>
 					</div>
 				  </li>
@@ -73,6 +73,7 @@
 		</div>
 		</footer>
 	
+
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
